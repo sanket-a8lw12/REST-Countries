fbb02dd38a23850be68970d5f9e7bd6ecacad7e7
Sanket <sanket.jagtap.24.2@mountblue.tech>
2023-07-25 14:22:11 +0530

    change the tag of country nam from h3 to h4

--- src/component/CountryCard.jsx
+++ src/component/CountryCard.jsx
@@ -3,14 +3,15 @@ import React from 'react'
 // import { Countries } from './Countries'
 
 export const CountryCard = ({countryFlag, countryName, population, region, capital}) => {
+  
   return (
     <>
-        <div className='countryCard'>
+        <div className='countryCard'> 
         <div className='countryImg'>
             <img src={countryFlag} alt="" />
         </div>
         <div className='countryDetails'>
-            <h3>{countryName}</h3>
+            <h4>{countryName}</h4>
             <div>Population : {population}</div>
             <div>Region : {region}</div>
             <div>Capital : {capital}</div>
