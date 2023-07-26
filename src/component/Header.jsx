725efc74e3ea78f3fb3929e7b6d99866a5eb1690
Sanket <sanket.jagtap.24.2@mountblue.tech>
2023-07-26 11:44:33 +0530

    added the dark mode

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
 
-export const Header = ({toggleStyle, myStyle} ) => {
+export const Header = ({ toggleStyle, myStyle }) => {
   return (
     <>
-    <div className='header' style={myStyle}>
+      <div className='header' style={myStyle}>
         <h2>Where in the world?</h2>
-        <button onClick={toggleStyle}>Dark Mode</button>
-    </div>
+        <button className='btn btn-warning' onClick={toggleStyle}>Dark Mode</button>
+      </div>
     </>
   )
 }
