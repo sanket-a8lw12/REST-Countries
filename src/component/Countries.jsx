729e0442593fff115ef2f721ee433c1649c85ff5
Sanket <sanket.jagtap.24.2@mountblue.tech>
2023-07-27 11:11:13 +0530

    added the error message if no search result is found

--- src/component/Countries.jsx
+++ src/component/Countries.jsx
@@ -1,14 +1,14 @@
-import  {CountryCard}  from './CountryCard'
+import { CountryCard } from './CountryCard'
 
-export const Countries = ({countries, myStyle}) => {
+export const Countries = ({ countries, myStyle }) => {
 
     return (
-        <div className='innerContainer'>
-
-        {countries.map((country)=>{
-            return < CountryCard countryFlag={country.flags.png} countryName={country.name.common} population={country.population}
-           region={country.region} capital={country.capital} key={country.name.common}/>
-        })}
+        <div className='innerContainer' style={myStyle}>
+            {countries.length === 0 ? <h1>Sorry no countries found...</h1> :
+                (countries.map((country) => {
+                    return < CountryCard countryFlag={country.flags.png} countryName={country.name.common} population={country.population}
+                        region={country.region} capital={country.capital} key={country.name.common} />
+                }))}
 
         </div>
     )
