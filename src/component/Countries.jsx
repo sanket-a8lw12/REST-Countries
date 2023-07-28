0846d2bf3aff6087a016d81e3b58e9abcafd2b41
Sanket <sanket.jagtap.24.2@mountblue.tech>
2023-07-28 16:16:44 +0530

    passed id = country.cca3 as props to Country card

--- src/component/Countries.jsx
+++ src/component/Countries.jsx
@@ -1,6 +1,7 @@
 import { CountryCard } from './CountryCard'
 import { useContext } from "react"
 import {ThemeContext} from './../App';
+import { Link } from 'react-router-dom';
 
 export const Countries = ({ countries, myStyle, error }) => {
     const [darkTheme, setDarkTheme] = useContext(ThemeContext);
@@ -11,9 +12,22 @@ export const Countries = ({ countries, myStyle, error }) => {
             {countries.length === 0 ? <h2>Sorry no countries found...</h2> :
                 (countries.map((country) => {
                     return < CountryCard countryFlag={country.flags.png} countryName={country.name.common} population={country.population}
-                        region={country.region} capital={country.capital} key={country.name.common}  />
+                        region={country.region} capital={country.capital} key={country.name.common}  id={country.cca3}/>
                 }))}
 
         </div>
+
+
+
+
+
+        //  <Link className= {darkTheme &&  'innerContainer darkMode' || 'innerContainer'} to="/Country/{}">
+        //  {countries.length === 0 ? <h2>Sorry no countries found...</h2> :
+        //      (countries.map((country) => {
+        //          return < CountryCard countryFlag={country.flags.png} countryName={country.name.common} population={country.population}
+        //              region={country.region} capital={country.capital} key={country.name.common}  id={country.cca3}/>
+        //      }))}
+
+        // </Link>
     )
 }
