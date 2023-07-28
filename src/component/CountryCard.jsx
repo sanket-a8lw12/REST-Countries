c55b3754d33fde2c4e8072c7d0f7b576d66213ea
Sanket <sanket.jagtap.24.2@mountblue.tech>
2023-07-28 16:18:06 +0530

    added the link so that on click it should land on particular page

--- src/component/CountryCard.jsx
+++ src/component/CountryCard.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
 import { useContext } from "react"
-import {ThemeContext} from './../App';
+import { ThemeContext } from './../App';
+import { Link } from 'react-router-dom';
 
-export const CountryCard = ({countryFlag, countryName, population, region, capital}) => {
+export const CountryCard = ({ countryFlag, countryName, population, region, capital, id }) => {
   const [darkTheme, setDarkTheme] = useContext(ThemeContext);
-  
+
   return (
     <>
-        <div className= {darkTheme &&  'countryCard darkMode' || 'countryCard'} > 
-        <div className='countryImg'>
+      <Link to={`/Country/${id}`}>
+        <div className={darkTheme && 'countryCard darkMode' || 'countryCard'} >
+          <div className='countryImg'>
             <img src={countryFlag} alt="" />
-        </div>
-        <div className='countryDetails'>
+          </div>
+          <div className='countryDetails'>
             <h4>{countryName}</h4>
             <div><b>Population : {population}</b></div>
             <div>Region : {region}</div>
             <div>Capital : {capital}</div>
+          </div>
         </div>
-        </div>
+      </Link>
     </>
   )
 }
