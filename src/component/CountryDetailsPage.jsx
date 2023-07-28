20d3e9b98fab5fd4716f7a5342215729f2f23539
Sanket <sanket.jagtap.24.2@mountblue.tech>
2023-07-28 16:19:19 +0530

    added the new component as CountryDetailPage with the detail infomration about the country

---
+++ src/component/CountryDetailsPage.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+import { Link } from 'react-router-dom';
+
+export default function CountryDetailsPage({countries}) {
+    const {id} = useParams()
+
+    const currentCountry = countries.find(country => country.cca3 === id);
+    console.log("-+-=-=-=-=-")
+    // console.log(Object.values(currentCountry.name.nativeName)[0].official);
+
+  return (
+    <>
+    <div>
+        <div>
+           <Link to={"/"}>
+           <button>Back Button</button>
+           </Link> 
+        </div>
+        <div>
+            <div>
+            <img src={currentCountry.flags.svg} alt="" />
+            </div>
+            <div>
+                <p>Name : {currentCountry.name.common}</p>
+        
+                <p>Native name : {Object.values(currentCountry.name.nativeName)[0].official }</p>
+
+            </div>
+
+            <div>
+
+            {currentCountry.borders.map((item)=>{
+                    return <Link to={`/Country/${item}`}><button key={item}>{item}</button> </Link>
+                    // console.log(item);
+                })}
+            </div>
+        </div>
+    </div>
+
+
+    </>
+  )
+}
