491da9c71e060f237da995a34f827c60932088c0
Sanket <sanket.jagtap.24.2@mountblue.tech>
2023-07-28 11:26:13 +0530

    removed the dropdown list from the searchbar and just imported the dropDown componenet

--- src/component/SearchBar.jsx
+++ src/component/SearchBar.jsx
@@ -1,70 +1,41 @@
 import { useContext } from "react"
-import {ThemeContext} from './../App';
+import { ThemeContext } from './../App';
+import { RegionDropDownList } from "./RegionDropDownList";
+import { SubRegionDropDownList } from "./SubRegionDropDownList";
+import { SortDropDownList } from "./SortDropDownList";
 
 export const SearchBar = ({ setRegion, searchResult, setSearchResult, subRegionList,
-    setSubRegion, setSortChange }) => {
+    setSubRegion, setSortChange, regionList }) => {
 
-        const [darkTheme, setDarkTheme] = useContext(ThemeContext);
+    const [darkTheme, setDarkTheme] = useContext(ThemeContext);
 
     const handleSearchResult = (event) => {
         setSearchResult(event.target.value)
     }
 
-    const handleRegionChange = (event) => {
-        setRegion(event.target.value);
-        setSubRegion('');
-    }
 
-    const handleSortChange = (event) => {
-        setSortChange(event.target.value);
-    }
-
-    const handleSubRegionChange = (event) => {
-        setSubRegion(event.target.value);
-    }
 
 
     return (
-        <div className= {darkTheme &&  'search darkMode' || 'search'}>
+        <div className={darkTheme && 'search darkMode' || 'search'}>
+
             <input onChange={handleSearchResult} className="searchBarWidth" type="search" placeholder="Search" aria-label="Search"
                 value={searchResult} />
 
-            <div className="dropdownOption">
-                <select id='dropdown' onChange={handleSortChange} className="searchBarWidth">
-                    <option value="">Sort</option>
-                    <option value="Ascending-population">Ascending population</option>
-                    <option value="Descending-population">Descending population</option>
-                    <option value="Ascending-area">Ascending area</option>
-                    <option value="Descending-area">Descending area</option>
-                </select>
-            </div>
+            < SortDropDownList
+                setSortChange={setSortChange}
+            />
 
+            < RegionDropDownList
+                setRegion={setRegion}
+                setSubRegion={setSubRegion}
+                regionList = {regionList}
+            />
 
-            <div className="dropdownOption">
-                <select id='dropdown' onChange={handleRegionChange} className="searchBarWidth">
-                    <option value="">Select Region</option>
-                    <option value="Oceania">Oceania</option>
-                    <option value="Americas">Americas</option>
-                    <option value="Asia">Asia</option>
-                    <option value="Europe">Europe</option>
-                    <option value="Africa">Africa</option>
-                    <option value="Antartica">Antartica</option>
-                </select>
-            </div>
-
-
-
-            <div className="dropdownOption">
-                <select onChange={handleSubRegionChange} className="searchBarWidth">
-                    <option value="">Select Subregion</option>
-                    {subRegionList.map((item) => {
-                        // console.log(item)
-                        // console.log("----------")
-                        return <option key={item} value={item}>{item}</option>
-                    })}
-                </select>
-            </div>
-
+            <SubRegionDropDownList
+                subRegionList={subRegionList}
+                setSubRegion={setSubRegion}
+            />
 
         </div>
     )
