39273f26fe5304630d4749697f2d698b847b2439
Sanket <sanket.jagtap.24.2@mountblue.tech>
2023-07-29 09:51:12 +0530

    removed the commented code

--- src/component/Countries.jsx
+++ src/component/Countries.jsx
@@ -4,6 +4,7 @@ import {ThemeContext} from './../App';
 import { Link } from 'react-router-dom';
 
 export const Countries = ({ countries, myStyle, error }) => {
+    
     const [darkTheme, setDarkTheme] = useContext(ThemeContext);
         
 
@@ -16,18 +17,5 @@ export const Countries = ({ countries, myStyle, error }) => {
                 }))}
 
         </div>
-
-
-
-
-
-        //  <Link className= {darkTheme &&  'innerContainer darkMode' || 'innerContainer'} to="/Country/{}">
-        //  {countries.length === 0 ? <h2>Sorry no countries found...</h2> :
-        //      (countries.map((country) => {
-        //          return < CountryCard countryFlag={country.flags.png} countryName={country.name.common} population={country.population}
-        //              region={country.region} capital={country.capital} key={country.name.common}  id={country.cca3}/>
-        //      }))}
-
-        // </Link>
     )
 }
