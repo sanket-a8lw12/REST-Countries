d581115e42bf020df02c9383556e5e3ca803ef2d
Sanket <sanket.jagtap.24.2@mountblue.tech>
2023-07-29 10:38:28 +0530

    added the population capital region and other details to the page

--- src/component/CountryDetailsPage.jsx
+++ src/component/CountryDetailsPage.jsx
@@ -16,28 +16,43 @@ export default function CountryDetailsPage({ countries }) {
     return (
         <>
             <div className= {darkTheme &&  'countryDetail darkMode' || 'countryDetail' }>
-                <div>
+                <div className='back-btn'>
                     <Link to={"/"}>
                         <button>Back Button</button>
                     </Link>
                 </div>
+
                 <div className='CountryInfo'>
                     <div>
                         <img src={currentCountry.flags.svg} alt="" />
                     </div>
-                    <div>
-                        <p>Name : {currentCountry.name.common}</p>
+                    <div className='CountryInfoText'>
+                        <h1>Name : {currentCountry.name.common}</h1>
 
                         <p>Native name : {Object.values(currentCountry.name.nativeName)[0].official}</p>
 
+                        <p>Population : {currentCountry.population}</p>
+
+                        <p>Capital : {currentCountry.capital}</p>
+
+                        <p>Sub-region : {currentCountry.subregion}</p>
+
+                        <p>Top Level Domain : {currentCountry.tld}</p>
+
+                        <p>Currency : {Object.values(currentCountry.currencies)[0].name}</p>
+
+                        <p>Language : {Object.values(currentCountry.languages).join(", ")}</p>
+
                     </div>
-
-                    <div>
-
+                    
+                    <div  className={darkTheme &&  'border darkMode' || 'border' }>
+                    <b>Border Countries</b>
+                        <div>
                         {currentCountry.borders.map((item) => {
-                            return <Link to={`/Country/${item}`}><button key={item}>{item}</button> </Link>
+                            return <Link to={`/Country/${item}`}><button className='border-btn' key={item}>{item}</button> </Link>
                             // console.log(item);
                         })}
+                        </div>
                     </div>
                 </div>
             </div>
