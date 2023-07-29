6db0accc1d70500949fdd655d66966a9daf36956
Sanket <sanket.jagtap.24.2@mountblue.tech>
2023-07-29 09:52:05 +0530

    removed the hardcoded list of subregion and now createing that list dynamically using map

--- src/component/RegionDropDownList.jsx
+++ src/component/RegionDropDownList.jsx
@@ -8,24 +8,15 @@ export const RegionDropDownList = ({ setRegion, setSubRegion, regionList }) => {
   }
   return (
 
-    // <div className="dropdownOption">
-    //   <select id='dropdown' onChange={handleRegionChange} className="searchBarWidth">
-    //     <option value="">Select Region</option>
-    //     <option value="Oceania">Oceania</option>
-    //     <option value="Americas">Americas</option>
-    //     <option value="Asia">Asia</option>
-    //     <option value="Europe">Europe</option>
-    //     <option value="Africa">Africa</option>
-    //     <option value="Antartica">Antartica</option>
-    //   </select>
-    // </div>
-
 <div className="dropdownOption">
 <select onChange={handleRegionChange} className="searchBarWidth">
+
     <option value="">Select Region</option>
+
     {regionList.map((item) => {
         return <option key={item} value={item}>{item}</option>
     })}
+    
 </select>
 </div>
   )
